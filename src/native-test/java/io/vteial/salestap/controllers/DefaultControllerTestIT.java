6e892414d50441a73485a4d8716ac284cc1c0f15
Eialarasu <vteial@gmail.com>
2022-04-20 15:06:44 +0530

    Initial commit

---
+++ src/native-test/java/io/vteial/salestap/controllers/DefaultControllerTestIT.java
@@ -0,0 +1,8 @@
+package io.vteial.salestap.controllers;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class DefaultControllerTestIT extends DefaultControllerTest {
+
+}
---
+++ src/test/java/io/vteial/salestap/controllers/DefaultControllerTest.java
@@ -0,0 +1,36 @@
+package io.vteial.salestap.controllers;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class DefaultControllerTest {
+
+    @ConfigProperty(name = "app.name")
+    private String appName = "-";
+
+    @Test
+    public void ping() {
+        given()
+                .when()
+                .get("/ping")
+                .then()
+                .statusCode(200)
+                .body(is("Ping Pong!!!"));
+    }
+
+    @Test
+    public void index() {
+        given()
+                .when()
+                .get("/index")
+                .then()
+                .statusCode(200)
+                .body(containsString(appName));
+    }
+}
